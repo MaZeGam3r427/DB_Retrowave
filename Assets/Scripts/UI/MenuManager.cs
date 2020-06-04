726bc67e220d636395fc15370ea21e756a0b0e27
MaZeGam3r427 <samuelarsac@hotmail.com>
2020-06-05 01:42:59 +0200

    Main Menu + Options Menu(WIP)

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject OptionsUI;
+    public GameObject MenuUI;
+
+
+    //Launch the game
+    public void Play()
+    {
+        SceneManager.LoadScene("HUB");
+    }
+
+    //Display the options
+    public void Options()
+    {
+        MenuUI.SetActive(false);
+        OptionsUI.SetActive(true);
+    }
+
+    //Hidding the options
+    public void Back()
+    {
+        MenuUI.SetActive(true);
+        OptionsUI.SetActive(false);
+    }
+
+    ////Change the volume through the value of the volume slider
+    //public void SetVolume(float volume)
+    //{
+    //    Debug.Log(volume);
+    //}
+
+    //Quit the game
+    public void Quit()
+    {
+        Debug.Log("Quitting application...");
+        Application.Quit();
+    }
+}
